13afc833859dcb34886475b696055db648f35d29
Judson Filgueira <judson.filgueira@gmail.com>
2026-04-18 11:24:47 -0300

    Lab - Terraform + Ansible com botão de Destroy

--- main.tf
+++ main.tf
@@ -25,8 +25,8 @@ resource "azurerm_public_ip" "public_ip" {
   name                = "public-ip-vm"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dinamic"
-  sku                 = "Standard"
+  allocation_method   = "Static"
+  sku                 = "Basic"
 }
 
 
